45dc3469b591ea6935cc893a969a4aed002a3975
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-09-21 11:40:47 +0600

    add route for category

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# Categories
+Route::get('/',[\App\Http\Controllers\CategoryController::class,'index']);
