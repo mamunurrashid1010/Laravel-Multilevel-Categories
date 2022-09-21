63ad491d23ecce0ec42a6a032a4f92de1b3ed533
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-09-21 11:42:04 +0600

    add index function for categories and implement

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+        $parentCategories = Category::where('parent_id',NULL)->get();
+        return view('categories', compact('parentCategories'));
+    }
 }
