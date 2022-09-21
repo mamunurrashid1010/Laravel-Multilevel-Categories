9a0a1fddc11b2626848da213c1c0d7ccf4fc187f
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-09-21 13:16:33 +0600

    add function tableView() and implement functinalities

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,8 +7,19 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    /**
+     * index function show categories list in list view
+     */
     public function index(){
         $parentCategories = Category::where('parent_id',NULL)->get();
         return view('categories', compact('parentCategories'));
+    }
+
+    /**
+     * tableView function show categories list in table view
+     */
+    public function tableView(){
+        $parentCategories = Category::where('parent_id',NULL)->get();
+        return view('categories-table', compact('parentCategories'));
     }
 }
